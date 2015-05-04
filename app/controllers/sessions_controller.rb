d09c8fa14b1275c4f08ca906d72a06bf5b099e4c
Amy Li <amyli@andrew.cmu.edu>
2015-05-04 19:05:11 -0400

    Made log in function work

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,12 +3,12 @@ class SessionsController < ApplicationController
     end
 
     def create
-      user = User.find_by_email(params[:email])
-      if user && User.authenticate(params[:email], params[:password])
+      user = User.find_by_username(params[:username])
+      if user && User.authenticate(params[:username], params[:password])
         session[:user_id] = user.id
         redirect_to home_path, notice: "Logged in!"
       else
-        flash.now.alert = "Email or password is invalid"
+        flash.now.alert = "Username or password is invalid"
         render "new"
       end
     end

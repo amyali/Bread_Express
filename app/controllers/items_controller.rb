fcd01a5f334eca8d6878a677c604647feba2d73c
Amy Li <amyli@andrew.cmu.edu>
2015-05-05 13:35:16 -0400

    modify items controller

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -8,12 +8,41 @@ class ItemsController < ApplicationController
   end
 
   def new
+    @item = Item.new
   end
 
   def show
   end
 
+  def create
+    @item = Item.new(item_params)
+
+    if @item.save
+      redirect_to items_path, notice: "The item was added to the system."
+    else
+      render action: 'new'
+    end
+  end
+
+  def update
+    if @item.update(item_params)
+      redirect_to items_path, notice: "The item was revised in the system."
+    else
+      render action: 'edit'
+    end
+  end
+
+  def destroy
+    @item.destroy
+    redirect_to items_url, notice: "The item was removed from the system"
+  end
+
   private
+
+  def set_item
+    @item = Item.find(params[:id])
+  end
+
   def item_params
     params.require(:item).permit(:name, :category, :units_per_item)
   end

c594e08fa234beef0a8810559a8c8184cbf32960
Amy Li <amyli@andrew.cmu.edu>
2015-05-05 02:30:23 -0400

    made changes to items controller

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,13 +1,20 @@
-class ItemController < ApplicationController
+class ItemsController < ApplicationController
   def edit
   end
 
   def index
+  	@active_items = Item.active.alphabetical.paginate(:page => params[:page]).per_page(10)
+  	@inactive_items = Item.inactive.alphabetical.paginate(:page => params[:page]).per_page(10)
   end
 
   def new
   end
 
   def show
+  end
+
+  private
+  def item_params
+    params.require(:item).permit(:name, :category, :units_per_item)
   end
 end

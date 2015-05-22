b862300567116e2c55bc5372a7d079a9be0b81c1
Amy Li <amyli@andrew.cmu.edu>
2015-05-22 16:17:32 -0400

    modified so customer show displays username correctly

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
+  authorize_resource
   # before_action :check_login, except: [:new]
   # before_action :set_user, only: [:show, :edit, :update, :destroy]
-  authorize_resource
 
   def new
     @user = User.new

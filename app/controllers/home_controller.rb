d0025d0d9c443493528eb0bec9966b9422a98b1a
Amy Li <amyli@andrew.cmu.edu>
2015-06-07 11:08:04 -0400

    some setup so bakers and shippers can make their lists?

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,6 +2,12 @@ class HomeController < ApplicationController
   include BreadExpressHelpers::Baking
 
   def home
+  	if logged_in? && !current_user.role?(:customer)
+  		  @bake_bread = create_baking_list_for("bread")
+        @bake_muffins = create_baking_list_for("muffins")
+        @bake_pastries = create_baking_list_for("pastries")
+        @shipping_list = Order.not_shipped.chronological
+    end
 
 
   end
